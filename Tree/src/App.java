7dd82a2a69dc772f175ac2bf679466ad2d092f48
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-27 13:22:49 +0530

    Insert

---
+++ Tree/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Tree BST = new Tree();
+        BST.add(56);
+        BST.add(30);
+        BST.add(70);
+    }
+}
---
+++ Tree/src/Node.java
@@ -0,0 +1,11 @@
+public class Node<T> {
+    public T value;
+    public Node<T> left;
+    public Node<T> right;
+
+    Node(T value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
