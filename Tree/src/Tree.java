9f787e0d1518267cee13244f574d81a6aae1d203
Roop Kumar Lala <ui20cs60@iiitsurat.ac.in>
2023-12-27 20:47:15 +0530

    Search & Print

--- Tree/src/Tree.java
+++ Tree/src/Tree.java
@@ -22,6 +22,14 @@ public class Tree {
         postorderFunction(this.root);
     }
 
+    public boolean find(Integer value){
+        return findFunction(this.root, value);
+    }
+
+    public int size(){
+        return sizeFunction(this.root);
+    }
+
     private void addFunction(Node<Integer> current, Integer insertValue){
         if(this.root == null){ this.root = new Node<Integer>(insertValue); return; }
 
@@ -35,27 +43,47 @@ public class Tree {
         }
     }
 
-    private void preorderFunction(Node<Integer> root){
-        if(root == null) return;
+    private void preorderFunction(Node<Integer> current){
+        if(current == null) return;
 
-        System.out.print(root.value + " ");
-        preorderFunction(root.left);
-        preorderFunction(root.right);
+        System.out.print(current.value + " ");
+        preorderFunction(current.left);
+        preorderFunction(current.right);
     }
 
-    private void inorderFunction(Node<Integer> root){
-        if(root == null) return;
+    private void inorderFunction(Node<Integer> current){
+        if(current == null) return;
         
-        inorderFunction(root.left);
-        System.out.print(root.value + " ");
-        inorderFunction(root.right);
+        inorderFunction(current.left);
+        System.out.print(current.value + " ");
+        inorderFunction(current.right);
     }
 
-    private void postorderFunction(Node<Integer> root){
-        if(root == null) return;
+    private void postorderFunction(Node<Integer> current){
+        if(current == null) return;
         
-        postorderFunction(root.left);
-        postorderFunction(root.right);
-        System.out.print(root.value + " ");
+        postorderFunction(current.left);
+        postorderFunction(current.right);
+        System.out.print(current.value + " ");
+    }
+
+    private int sizeFunction(Node<Integer> current){
+        if(current == null) return 0;
+        
+        return sizeFunction(current.left) + sizeFunction(current.right) + 1;
+    }
+
+    private boolean findFunction(Node<Integer> current, Integer findValue){
+        if(this.root == null){ return false; }
+        if(current.value == findValue) return true;
+
+        if(current.value > findValue){
+            if(findFunction(current.left, findValue)) return true;
+        }
+        else{
+            if(findFunction(current.right, findValue)) return true;
+        }
+
+        return false;
     }
 }
